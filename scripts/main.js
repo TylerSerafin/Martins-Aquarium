6596298e2f9aa07fd38c23f60dab774c5c49eb03
TylerSerafin <tyler.serafin2@gmail.com>
2020-02-27 15:20:24 -0600

    Added fish automation array

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import initializeDetailButtonEvents from './dialogs.js'
+import { useFish } from './Fish/FishDataProvider.js'
+import FishList from './Fish/FishList.js'
+
+// initializeDetailButtonEvents()
+
+FishList()
+
